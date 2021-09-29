a77bd8810d1224b145c7f58d57f8671855867093
Alexandr <ak4raulov@yandex.ru>
2021-09-29 18:10:41 +0300

    таймер

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 window.addEventListener('DOMContentLoaded', () => {
-
+//Табы
     const tabs = document.querySelectorAll('.tabheader__item');
     const tabsContent = document.querySelectorAll('.tabcontent');
     const tabsParent = document.querySelector('.tabheader__items');
@@ -34,5 +34,54 @@ window.addEventListener('DOMContentLoaded', () => {
                 }
             });
         }
-    });
+	 });
+	//Таймер
+/**1.функция которая будет устанавливать таймер(получить элементы и что то с ними делать) 
+2. функционал который будет определять разницу между временем пользователь и датой окончания таймера
+3. функция которая будет заниматся обномвлением таймера
+**/
+	//const deadLine = '2021-09-30';
+	//function getTimeRemaining(endtime) {
+	//	const t = Date.parse(endtime) - Date.parse(new Date());
+	//	const days = Math.floor(t / (1000 * 60 * 60 * 24));
+	//	const hours = Math.floor((t / (1000 * 60 * 60) % 24));
+	//	const minutes = Math.floor((t / 1000 / 60) % 60);
+	//	const seconds = Math.floor((t / 1000) % 60);
+	//	return {
+	//		'total': t,
+	//		'days': days,
+	//		'hours': hours,
+	//		'minutes': minutes,
+	//		'seconds': seconds
+	//	};
+	//}
+
+	//function getZero(num) {
+	//	if (num >= 0 && num < 10) {
+	//		return `0${num}`;
+	//	} else {
+	//		return num;
+	//	}
+	//}
+
+	//function setClock(selector, endtime) {
+	//	const timer = document.querySelector(selector);
+	//	const days = timer.querySelector('#days');
+	//	const hours = timer.querySelector('#hours');
+	//	const minutes = timer.querySelector('#minutes');
+	//	const seconds = timer.querySelector('#seconds');
+	//	const timeInterval = setInterval(updateClock, 1000);
+	//	updateClock();
+	//	function updateClock() {
+	//		const t = getTimeRemaining(endtime);
+	//		days.innerHTML = getZero(t.days);
+	//		hours.innerHTML = getZero(t.hours);
+	//		minutes.innerHTML = getZero(t.minutes);
+	//		seconds.innerHTML = getZero(t.seconds);
+	//		if (t.total <= 0) {
+	//			clearInterval(timeInterval);
+	//		}
+	//	}
+	//}
+	//setClock('.timer', deadLine);
 });
---
+++ js/timer.js
@@ -0,0 +1,51 @@
+/**1.функция которая будет устанавливать таймер(получить элементы и что то с ними делать) 
+2. функционал который будет определять разницу между временем пользователь и датой окончания таймера
+3. функция которая будет заниматся обномвлением таймера
+**/
+const stock = '2021-10-30';
+
+function getTimeDifference(endtime) {
+	const t = Date.parse(endtime) - Date.parse(new Date());
+	const days = Math.floor(t / (1000 * 60 * 60 * 24));
+	const hours = Math.floor((t / (1000 * 60 * 60) )% 24);
+	const minutes = Math.floor((t / (1000 * 60)) % 60);
+	const seconds = Math.floor((t / 1000) % 60);
+	return {
+		'total': t,
+		'days': days,
+		'hours': hours,
+		'minutes': minutes,
+		'seconds': seconds
+	};
+}
+function zero(num) {
+	if (num < 10) {
+		return `0${num}`;
+	} else {
+		return num;
+	}
+}
+//console.log(getTimeRemaining(deadLine));
+
+function setClock() {
+	const days = document.querySelector('#days');
+	const hours= document.querySelector('#hours');
+	const minutes = document.querySelector('#minutes');
+	const seconds = document.querySelector('#seconds');
+	const timeInterva = setInterval(updateClock, 1000);
+	updateClock();
+	function updateClock () {
+		const t = getTimeDifference(stock);
+		days.innerHTML = zero(t.days);
+		hours.innerHTML = zero(t.hours);
+		minutes.innerHTML = zero(t.minutes);
+		seconds.innerHTML = zero(t.seconds);
+		if (t <= 0) {
+			clearInterval(timeInterva);
+		}
+	}
+}
+
+setClock();
+
+
